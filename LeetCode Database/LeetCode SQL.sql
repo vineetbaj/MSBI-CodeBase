f5ee461373f25af0096dc9aef9dad3a9724cd921
Vineet Baj <vineetbaj@gmail.com>
2019-02-19 15:01:58 +0530

    Update LeetCode SQL.sql

--- LeetCode Database/LeetCode SQL.sql
+++ LeetCode Database/LeetCode SQL.sql
@@ -6,3 +6,24 @@
 select FirstName, LastName, City, State
 from Person p
 left join Address a on p.PersonId=a.PersonId
+
+--Second highest
+with cte as (
+ select top 2 Salary from Employee
+    order by Salary desc
+)select top 1 Salary from cte
+order by Salary
+
+--Nth highest Salary
+CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
+BEGIN
+    RETURN (
+     select distinct Salary
+          from 
+           (select dense_rank() over (order by salary desc) as Ranks, ID, Salary
+            from Employee) as a
+            where a.Ranks = @N
+    
+    )
+        
+END
